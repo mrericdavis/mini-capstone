dddd8cf98f9725c40d14d9a77eaa8244a9e94024
Eric Davis <Topcoder10000@gmail.com>
2017-07-06 13:12:44 -0500

    added features to about us and contact us page

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -3,6 +3,25 @@ class ShoesController < ApplicationController
     @shoes = Shoe.all
     render "shoe.html.erb"
   end
+  def mens_shoe_method
+    render "menshoe.html.erb"
+  end  
+  def ladies_shoe_method
+
+    render "ladyshoe.html.erb"
+  end
+  def kids_shoe_method
+
+    render "kidshoe.html.erb"
+  end
+  def about_us_method
+
+    render "aboutus.html.erb"
+  end
+  def contact_us_method
+
+    render "contactus.html.erb"
+  end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get "/shoestore" => "shoes#shoes_method"
+  get "/menshoe.html.erb" => "shoes#mens_shoes_method"
+  get "/ladyshoe.html.erb" => "shoes#ladies_shoes_method"
+  get "/kidshoe.html.erb" => "shoes#kids_shoes_method"
+  get "/aboutus.html.erb" => "shoes#about_us_method"
+  get "/contactus.html.erb" => "shoes#contact_us_method"
 end
