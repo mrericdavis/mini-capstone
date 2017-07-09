fca4300aea724783fe7aab12a0e69430c566b631
Eric Davis <Topcoder10000@gmail.com>
2017-07-09 13:11:31 -0500

    addedsome coding for kids profile

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,8 +1,21 @@
 class ShoesController < ApplicationController
+  def home
+    @shoes = [Shoe.first, Shoe.second, Shoe.third]
+    render "home.html.erb"
+  end
+
   def index
     @shoes = Shoe.all
     render "index.html.erb"
   end
+
+  def show
+    shoes_id = params[:id]
+    @shoe = Shoe.find_by(id: shoes_id)
+    render "show.html.erb"
+  end  
+
+
   def mens_shoes_method
     render "menshoe.html.erb"
   end  
@@ -11,7 +24,9 @@ class ShoesController < ApplicationController
     render "ladyshoe.html.erb"
   end
   def kids_shoes_method
-
+    @shoe = Shoe.find_by(id:4)
+    @shoe = Shoe.find_by(id:5)
+    @shoe = Shoe.find_by(id:6)
     render "kidshoe.html.erb"
   end
   def about_us_method
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get "/" => "shoes#index"
+  get "/" => "shoes#home"
   get "/shoes" => "shoes#index"
+  get "/shoes/:id" => "shoes#show"
+
   get "/menshoe.html.erb" => "shoes#mens_shoes_method"
   get "/ladyshoe.html.erb" => "shoes#ladies_shoes_method"
   get "/kidshoe.html.erb" => "shoes#kids_shoes_method"
   get "/aboutus.html.erb" => "shoes#about_us_method"
   get "/contactus.html.erb" => "shoes#contact_us_method"
+  get "/shoes/:id" => "shoes#show"
+
 end
