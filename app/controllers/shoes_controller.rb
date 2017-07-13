329c458a5db4ab16bad57596b7ce9f153eabc6f6
Eric Davis <Topcoder10000@gmail.com>
2017-07-12 19:35:20 -0500

    changed some css

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -3,40 +3,83 @@ class ShoesController < ApplicationController
     @shoes = [Shoe.first, Shoe.second, Shoe.third]
     render "home.html.erb"
   end
-
+  
+  def edit
+    shoe_id = params[:id]
+    @shoe = Shoe.find_by(id: shoe_id)
+    render "edit.html.erb"
+  end
+  
   def index
     @shoes = Shoe.all
     render "index.html.erb"
   end
 
   def show
-    shoes_id = params[:id]
-    @shoe = Shoe.find_by(id: shoes_id)
+    # shoes_id = params[:id]
+    @shoe = Shoe.find_by(id: params[:id])
     render "show.html.erb"
   end  
 
 
   def mens_shoes_method
+    @shoe1 = Shoe.find_by(id:1)
+    @shoe2 = Shoe.find_by(id:2)
+    @shoe3 = Shoe.find_by(id:3)
     render "menshoe.html.erb"
   end  
+  
+  def new
+    render "new.html.erb"
+  end  
+  
   def ladies_shoes_method
-
     render "ladyshoe.html.erb"
   end
+
   def kids_shoes_method
-    @shoe = Shoe.find_by(id:4)
-    @shoe = Shoe.find_by(id:5)
-    @shoe = Shoe.find_by(id:6)
+    @shoe1 = Shoe.find_by(id:4)
+    @shoe2 = Shoe.find_by(id:5)
+    @shoe3 = Shoe.find_by(id:6)
     render "kidshoe.html.erb"
   end
-  def about_us_method
 
+  def about_us_method
     render "aboutus.html.erb"
   end
-  def contact_us_method
 
+  def create
+    shoe = Shoe.new(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description]
+     )
+    shoe.save
+    render "create.html.erb"
+  end
+
+  def update
+    shoe_id = params[:id]
+    @shoe = Shoe.find_by(id: shoe_id)
+    @shoe.name = params[:name]
+    @shoe.price = params[:price]
+    @shoe.image =  params[:image]
+    @shoe.description = params[:description]
+    @shoe.save
+    render "update.html.erb"
+  end  
+
+  def contact_us_method
     render "contactus.html.erb"
   end
+
+  def destroy
+    shoes_id = params[:id]
+    @shoe = Shoe.find_by(id: shoes_id)
+    @shoe.destroy
+    render "destroy.html.erb"
+  end  
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   get "/" => "shoes#home"
   get "/shoes" => "shoes#index"
+  get "/shoes/new" => "shoes#new"
+  post "/shoes" => "shoes#create"
   get "/shoes/:id" => "shoes#show"
-
+  get "/shoes/:id/edit" => "shoes#edit"
+  patch "/shoes/:id" => "shoes#update"
+  delete "/shoes/:id" => "shoes#destroy"
+  
   get "/menshoe.html.erb" => "shoes#mens_shoes_method"
   get "/ladyshoe.html.erb" => "shoes#ladies_shoes_method"
   get "/kidshoe.html.erb" => "shoes#kids_shoes_method"
   get "/aboutus.html.erb" => "shoes#about_us_method"
   get "/contactus.html.erb" => "shoes#contact_us_method"
-  get "/shoes/:id" => "shoes#show"
-
 end
