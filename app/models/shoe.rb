554c7526dabef83b433dfb251bc7e16b191d84f0
Eric Davis <Topcoder10000@gmail.com>
2017-07-12 21:01:34 -0500

    added sales price

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,2 +1,20 @@
 class Shoe < ApplicationRecord
+
+  def sales_message
+    if price < 130.00
+      return "Discount Item!!!"
+    else 
+      return "Everyday Value!!!"
+    end 
+  end    
+
+  def tax
+    return price * 0.09
+  end 
+
+  def total
+    return price + tax
+  end
+
+
 end
