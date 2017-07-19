cf94f3ffaf605d7d4a5ecea9e95de0c0730ab3b8
Actualize <actualize@Actualizes-MacBook-Air.local>
2017-07-19 18:34:44 -0500

    Add supplier model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Shoe.create(name: "Jordans", price: 150.00, image: "https://newsneakernews-wpengine.netdna-ssl.com/wp-content/uploads/2017/02/stadium-goods-best-selling-air-jordans-february-2017-10.jpg", description: "these are jordans")
+# Shoe.create(name: "Futures", price: 100.00, image: "https://hir.ma/wp-content/uploads/2015/10/back-to-the-future-2015-nike-air-mag-shoe-gif-3.gif", description: "these are crazy")
+# Shoe.create(name: "Lebrons", price: 100.00, image: "https://s7d2.scene7.com/is/image/dkscdn/17NIKMLBRN14RDBLKPRF_Red_Black_is/", description: "these are ugly")
+# Shoe.create(name: "Samoa", price: 80.00, image: "http://scene7.zumiez.com/is/image/zumiez/pdp_hero/adidas-Samoa-Black-%26-Purple-Women-s-Shoes-_270527-front.jpg", description: "these are ok")
+
+
+# Supplier.create(name: "Nike", email: "Nike@yahoo.com", phone_number:"312-789-3214")
+# Supplier.create(name: "Adidas", email: "Adidas@yahoo.com", phone_number:"773-923-2328")
