786b1ac23f208d33bc790c169043362bb6cda459
Eric Davis <Topcoder10000@gmail.com>
2017-07-19 18:34:59 -0500

    changed background

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'therubyracer', platforms: :ruby
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'jquery-rails', '~> 4.3.1' 
-
+gem 'seed_dump'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Shoe.create!([
+  {name: "Air Max", price: 129.99, image: "http://www.shoesporting.com/images/sportingshoes/Nike%20Air%20Max%20Mens/Air%20Max%2090%20Mens%20Shoes/Air%20Max%2090%20Running%20Shoes/Air_Max_90_Mens_Shoes_Cheap_Wholesale_Black_White_Red_Grey.jpg", description: "Black red and white nike"},
+  {name: "Air_Force_One", price: 109.99, image: "http://i.ebayimg.com/00/s/NDgwWDY0MA==/z/QKcAAOSwq7JTz4Qt/$_32.JPG", description: "The shoes that will put you in Check"},
+  {name: "Air Force One", price: 75.99, image: "http://images.footlocker.com/pi/96728026/large/nike-air-force-1-low-boys-grade-school", description: "Kids' Air Force Ones"},
+  {name: "Timberland", price: 149.99, image: "https://images.timberland.com/is/image/timberland/10061024-HERO?$CATEGORY-LANDING-PRODUCT-REC$", description: "Winter Boots"},
+  {name: "Jordan", price: 79.99, image: "http://www.airjordansshoesus.org/images/jordan/Kids-Air-Jordan-13-Black-Red_2.jpg", description: "Kids Air Jordans"},
+  {name: "Gucci Shoes", price: 4999.99, image: "https://s-media-cache-ak0.pinimg.com/originals/b7/ce/b8/b7ceb82046472bc556560ed7bee41f5e.jpg", description: "Gucci Shoes"},
+  {name: "Prada", price: 2999.99, image: "https://s-media-cache-ak0.pinimg.com/736x/1b/88/ca/1b88ca4e17c70dd23216ef73a4fcc6f5--prada-shoes-men-prada-men.jpg", description: "Prada Shoe"},
+  {name: "Hroyl", price: 87.99, image: "https://ae01.alicdn.com/kf/HTB12APuPVXXXXcEXVXXq6xXFXXXu/New-arriver-Tango-dancing-girls-Women-ballroom-font-b-Latin-b-font-font-b-shoes-b.jpg", description: "Nice Brown Heels"},
+  {name: "Vanichi", price: 299.99, image: "http://www.dhresource.com/260x260s/f2-albu-g5-M00-DA-95-rBVaJFiljpqARhgbAAL-JWo9B1c030.jpg/high-quality-office-ladie-dress-shoes-high.jpg", description: "Black and blue Heels"},
+  {name: "Howen Lew ", price: 99.99, image: "https://ae01.alicdn.com/kf/HTB1ygbwQFXXXXXbXVXXq6xXFXXXm/Free-Shipping-font-b-Latin-b-font-salsa-font-b-shoes-b-font-font-b-lady.jpg", description: "Rainbow Collection"},
+  {name: "Air Jordan", price: 149.99, image: "http://images.solecollector.com/complex/image/upload/f_auto,fl_lossy,q_auto,w_1200/air-jordan-11-retro-playoffs-2012_pqquyy", description: "Michael Jordon's signature shoe"},
+  {name: "Converse", price: 49.99, image: "https://s-media-cache-ak0.pinimg.com/736x/0a/87/12/0a87129d9b1efe26f4d6b9695ae69305--kids-converse-converse-chucks.jpg", description: "Kids Converse"}
+])
